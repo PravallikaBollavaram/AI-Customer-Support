cca74a9efb1e3577a4dfa1c780f9fb8e23b48476
Pravallika Bollavaram <pravallikabollavaram123@gmail.com>
2024-08-28 13:31:18 -0500

    Signout changes

--- app/signout/page.js
+++ app/signout/page.js
@@ -1,31 +1,12 @@
-"use client"; // Mark this file as a client component
+// app/signout/page.js
 
-import { signOut, onAuthStateChanged } from "firebase/auth";
-import { auth } from "@/app/firebase/config";
+import dynamic from 'next/dynamic';
 
-// Function to sign out the user
-export const signOutUser = async () => {
-  if (typeof window === "undefined") {
-    // Ensure this code only runs in the client environment
-    return;
-  }
+// Dynamically import the SignOutComponent with no SSR
+const SignOutComponent = dynamic(() => import('@/app/signout/SignOutComponent'), {
+  ssr: false,
+});
 
-  try {
-    await signOut(auth);
-    console.log("User signed out successfully");
-    // Redirect the user to the login page or home page, if necessary
-    // For example: window.location.href = "/login";
-  } catch (error) {
-    console.error("Error signing out: ", error);
-  }
-};
-
-// Function to listen to authentication state changes
-export const getAuthState = (callback) => {
-  if (typeof window === "undefined") {
-    // Ensure this code only runs in the client environment
-    return;
-  }
-
-  return onAuthStateChanged(auth, callback);
-};
+export default function SignOutPage() {
+  return <SignOutComponent />;
+}
